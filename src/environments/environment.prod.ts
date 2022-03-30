bfba01922f5a5c185c48cf880b8aa832cfb39694
AndreiAzaronak <aazaronak@gmail.com>
2022-03-30 20:03:35 +0300

    изменения

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiKey: "AIzaSyCxAs7WYEgcqhgI_CuYeUEvrlQfjKC_xGM",
+  dbUrl: "https://ang-shop-48f1e-default-rtdb.europe-west1.firebasedatabase.app"
+};
